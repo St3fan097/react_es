2e2a65c2ceef20d6dfd69a8ecaf8e2745e5c3c77
St3fan097 <Ste97.esse@gmail.com>
2023-11-05 21:56:16 +0100

    exercise-9

--- react-es_1/src/welcome.jsx
+++ react-es_1/src/welcome.jsx
@@ -5,11 +5,8 @@ export function Welcome({name, age}) {
     return (
         <div>
             <strong>Welcome, {name || "Guest"}!</strong>
-            <p>Your age is {age}</p>
-            {age > 18 && <p>Your age is {age}</p>}
-            {age !== undefined && <p>Your age is {age}</p>}
-            {(age > 18 && age < 65) && <p>Your age is {age}</p>}
-            {age > 18 && name === "Stefano" && <p>Your age is {age}</p>}   {/*al posto di John ho messo il mio nome */}
+            {age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}
+
         </div>
     )
 }
