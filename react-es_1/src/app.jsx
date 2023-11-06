d53e1b8f5176e580803ea23374bc8742bd1aba60
St3fan097 <Ste97.esse@gmail.com>
2023-11-06 18:56:17 +0100

    exercise-16

--- react-es_1/src/app.jsx
+++ react-es_1/src/app.jsx
@@ -10,7 +10,7 @@ export function App() {
         <div>
         <Counter valoreIniziale={0} incremento={1} />  
         <Clock/>
-        <MouseClicker name="uno"/>
+        <MouseClicker name="uno" src="aye.jpg"/>
         </div>
     )
 }
--- react-es_1/src/mouseClicker.jsx
+++ react-es_1/src/mouseClicker.jsx
@@ -1,11 +1,16 @@
-export function MouseClicker({name}) {
+export function MouseClicker({ name, src}) {
     function stampa(event) {
-        console.log(event.target.name);
-        
+        console.log(event.target.name)
+    }
+    function stampaImg(event){
+        console.log(event.target.src); // non ho capito perchè non trova l'immagine ma dovrebbe funzionare nel caso
     }
     return (
         <div>
-            <button name={name} onClick={stampa}>Clicca qui</button>
+            <button name={name} onClick={stampa}>
+                <img src={src} alt="eye" onClick={stampaImg} /> 
+                Clicca qui
+            </button>
         </div>
     )
 }
