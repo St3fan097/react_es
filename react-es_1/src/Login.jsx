b3c3f9e1840d649026cb15613a7a78bed9939fc0
St3fan097 <Ste97.esse@gmail.com>
2023-11-09 13:02:52 +0100

    exercise-19

--- react-es_1/src/Login.jsx
+++ react-es_1/src/Login.jsx
@@ -15,12 +15,23 @@ function Login() {
         setRemember(el.target.checked)
     }
 
+    function onLogin() {
+        if (userName !== "" && password !== "") {
+            userName, password, remember;
+        }
+    }
+
+
 
     return (
         <>
-            <input type="text" value={userName} onChange={text}/>
-            <input type="password" value={password} onChange={passwordFunc}/>
+            <input type="text" placeholder="Inserisci Username" value={userName} onChange={text}/>
+            <input type="password" placeholder="Password" value={password} onChange={passwordFunc}/>
+            <label style={{ width: "120px", display: "flex"}}> Remember me!
             <input type="checkbox" value={remember} onClick={rememberMe}/>
+            </label>
+            <hr></hr>
+            <button onClick={onLogin} disabled={userName === "" || password === "" || remember == false}>Login</button>
         </>
     )
 }
