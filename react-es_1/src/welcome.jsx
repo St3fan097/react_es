d3119bc552246ab10353c8ee2848bc1c783adbee
St3fan097 <Ste97.esse@gmail.com>
2023-11-09 12:09:09 +0100

    fix:exercise-9

--- react-es_1/src/welcome.jsx
+++ react-es_1/src/welcome.jsx
@@ -1,11 +1,11 @@
 
-/* import { Age } from './age'; */
+import { Age } from './age';
 
 export function Welcome({name, age}) {
     return (
         <div>
             <strong>Welcome, {name || "Guest"}!</strong>
-            {age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}
+            <Age age={age}/>
 
         </div>
     )
