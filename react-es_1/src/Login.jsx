8e24ee828c82f736e12a3b604b1e7b2f5b63208d
St3fan097 <Ste97.esse@gmail.com>
2023-11-09 13:50:02 +0100

    exercise-20

--- react-es_1/src/Login.jsx
+++ react-es_1/src/Login.jsx
@@ -5,19 +5,27 @@ function Login() {
     const [password, setPassword] = useState("");
     const [remember, setRemember] = useState(false);
 
-    function text (el) {
+    function text(el) {
         setUserName(el.target.value)
     }
-    function passwordFunc (el) {
+    function passwordFunc(el) {
         setPassword(el.target.value)
     }
-    function rememberMe (el) {
+    function rememberMe(el) {
         setRemember(el.target.checked)
     }
 
     function onLogin() {
-        if (userName !== "" && password !== "") {
+        if (userName !== "" && password !== "" && remember) {
             userName, password, remember;
+        }
+    }
+
+    function resetMe(el) {
+        if (userName !== "" && password !== "" && remember) {
+            setUserName("");
+            setPassword("");
+            setRemember(false);
         }
     }
 
@@ -25,15 +33,18 @@ function Login() {
 
     return (
         <>
-            <input type="text" placeholder="Inserisci Username" value={userName} onChange={text}/>
-            <input type="password" placeholder="Password" value={password} onChange={passwordFunc}/>
-            <label style={{ width: "120px", display: "flex"}}> Remember me!
-            <input type="checkbox" value={remember} onClick={rememberMe}/>
+            <input type="text" placeholder="Inserisci Username" value={userName} onChange={text} />
+            <input type="password" placeholder="Password" value={password} onChange={passwordFunc} />
+            <label style={{ width: "120px", display: "flex" }}> Remember me!
+                <input type="checkbox" checked={remember} onClick={rememberMe} />
             </label>
             <hr></hr>
             <button onClick={onLogin} disabled={userName === "" || password === "" || remember == false}>Login</button>
+            <button onClick={resetMe}>Reset</button>
         </>
     )
 }
+
+//funziona tutto ma non ho capito perchè nel browser ci sia un errore dove dice che login non è controllato
 
 export default Login
