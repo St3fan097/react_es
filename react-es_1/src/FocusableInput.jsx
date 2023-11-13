e096b9140a0f7de01e00bac016fb8cc1b6bddd82
St3fan097 <Ste97.esse@gmail.com>
2023-11-13 15:04:02 +0100

    exercise-25

--- react-es_1/src/FocusableInput.jsx
+++ react-es_1/src/FocusableInput.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useRef } from "react"
 
-export function FocusableInput () {
+export function FocusableInput() {
 
     const inputRef = useRef(null)
+    const renderingRef = useRef(false)
 
     useEffect(() => {
+        if (!renderingRef.current) {
+            renderingRef.current = true
+            console.log("Montato una volta")
+        } else {
+            console.log("Montato due volte")
+        }
         inputRef.current?.focus()
-    },[])
+    }, [])
 
     return (
-        <input ref={inputRef} type="text"/>
+        <input ref={inputRef} type="text" />
     )
 }
--- react-es_1/src/index.jsx
+++ react-es_1/src/index.jsx
@@ -1,6 +1,11 @@
+import { StrictMode } from "react";
 import { App } from "./app";
-import {createRoot} from "react-dom/client"
+import { createRoot } from "react-dom/client"
 
 
-createRoot(document.getElementById("root")).render(<App/>)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
 
