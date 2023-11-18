6affd70dd882936dc8ce065f48d70a6a507a1e58
St3fan097 <Ste97.esse@gmail.com>
2023-11-18 16:40:06 +0100

    exercise-37

--- react-es_1/src/app.jsx
+++ react-es_1/src/app.jsx
@@ -1,5 +1,7 @@
 
+import { useState } from "react";
 import { Container } from "./Container";
+import { LanguageContext } from "./LanguageContext";
 import { TodoList } from "./TodoList";
 import { Clock } from "./clock";
 import { Counter } from "./counter";
@@ -10,12 +12,24 @@ import { Welcome } from "./welcome";
 
 export function App() {
 
+
+    const [language, setLanguage] = useState("en")
+
+    function switchLanguage(language) {
+        setLanguage(language)
+    }
+
     return (
-        <Container title={<h1 style={{margin: 0}}>Pagina con dettagli</h1>}>
-            <Welcome/>
+        <Container title={<h1 style={{ margin: 0 }}>Pagina con dettagli</h1>}>
+            <Welcome />
             <Counter valoreIniziale={0} incremento={1} />
-            <Clock />
-           <TodoList/>
+            <LanguageContext.Provider value={language}>
+                <Clock />
+                <button onClick={() => { switchLanguage("en") }}>EN</button>
+                <button onClick={() => { switchLanguage("it") }}>IT</button>
+                <hr />
+            </LanguageContext.Provider>
+            <TodoList />
         </Container>
     )
 }
--- react-es_1/src/clock.jsx
+++ react-es_1/src/clock.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export function Clock() {
     const [date, setDate] = useState(new Date())
@@ -10,7 +11,9 @@ export function Clock() {
         }, 1000)
     }, []);
 
+const language = useContext(LanguageContext)
+
     return (
-        <p className="clock">Sono le {date.toLocaleTimeString()}</p>
+        <p className="clock">{language === "en" ? "Current time" : "Orario corrente"} {date.toLocaleTimeString()}</p>
     )
 }
