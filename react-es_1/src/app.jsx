1e2a486df6425ea1dbb7f2f24fd65fd776de67f1
St3fan097 <Ste97.esse@gmail.com>
2023-11-25 11:52:54 +0100

    exercise-47

--- react-es_1/src/app.jsx
+++ react-es_1/src/app.jsx
@@ -9,14 +9,16 @@ import { Welcome } from "./welcome";
 import { GithubUser } from "./GithubUser";
 import Login from "./Login";
 import { useCurrentLocation } from "./useCurrentLocation";
-import { FilteredList } from "./FilteredList";
+import { Route, Routes } from "react-router-dom";
+
+
 
 
 
 
 export function App() {
 
-    const  {location,error,loading, getCurrentPosition } = useCurrentLocation()
+    const { location, error, loading, getCurrentPosition } = useCurrentLocation()
 
 
 
@@ -28,9 +30,11 @@ export function App() {
 
     return (
         <Container title={<h1 style={{ margin: 0 }}>Pagina con dettagli</h1>}>
-            <Welcome />
-            <GithubUser  username="Stefano"/>
-            <Counter initialValue={0}/>
+            <Routes>
+                <Route path="/" element={<Welcome name="Stefano"/>} />
+            </Routes>
+            <GithubUser username="Stefano" />
+            <Counter initialValue={0} />
             <LanguageContext.Provider value={language}>
                 <Clock />
                 <button onClick={() => { switchLanguage("en") }}>EN</button>
@@ -38,15 +42,13 @@ export function App() {
                 <hr />
             </LanguageContext.Provider>
             <TodoList />
-            <Login/>
-        <div>
-             {loading &&  <p>Loading</p>}
+            <Login />
+            <div>
+                {loading && <p>Loading</p>}
                 {error && <p>Error</p>}
                 {location && <p>{location.latitude}, {location.longitude}</p>}
                 <button onClick={getCurrentPosition}>Position</button>
-        </div>
-        <FilteredList/>
+            </div>
         </Container>
-
     )
 }
--- react-es_1/src/index.jsx
+++ react-es_1/src/index.jsx
@@ -1,8 +1,8 @@
-import { App } from "./app";
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import "./index.scss"
+import { Root } from "./Root";
 
-createRoot(document.getElementById("root")).render(<App/>)
+createRoot(document.getElementById("root")).render(<Root/>)
 
