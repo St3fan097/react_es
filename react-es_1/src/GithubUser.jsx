b9630bc3000d58c02ed511d85d8db76eee2ebd9a
St3fan097 <Ste97.esse@gmail.com>
2023-11-29 12:01:07 +0100

    fix_exercise_54

--- react-es_1/src/GithubUser.jsx
+++ react-es_1/src/GithubUser.jsx
@@ -1,67 +1,45 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import useSWR from "swr";
 
-function useGithubUser () {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const [username, setUsername] = useState("")
-    const [usernameInput, setUsernameInput] = useState("")
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
-    useEffect(() => {
+function useGithubUser() {
+    const [username, setUsername] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    const { data, error } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher);
 
-        if (!username) {
-            return;
-        }
-        setLoading(true)
-        setError(null)
-
-        fetch(`https://api.github.com/users/${username}`)
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json)
-                setLoading(false)
-                setData((prevData) => [...prevData, json])
-
-            })
-            .catch((error) => setError(error))
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [username])
-
-    function insertName(event) {
-        setUsernameInput(event.target.value)
-    }
-
-    function fetchData() {
-        setUsername(usernameInput);
-    }
-    return{
-        data, loading, error, username, usernameInput, insertName , fetchData
-    }
+    return {
+        user: data,
+        loading: inputValue !== "" && (!data && !error),
+        isError: error,
+        inputValue,
+        setUsername,
+        setInputValue,
+    };
 }
 
 export function GithubUser() {
-    const { data, loading, error, username, usernameInput, insertName, fetchData } = useGithubUser()
+    const { user, loading, isError, inputValue, setUsername, setInputValue } = useGithubUser();
 
+    const fetchData = () => {
+        if (inputValue) {
+            setUsername(inputValue);
+        }
+    };
 
     return (
         <>
-            <input type="text" placeholder="Insert username" value={usernameInput} onChange={insertName} />
+            <input type="text" placeholder="Insert username" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             <button onClick={fetchData}>Genera</button>
             {loading && <h1>Loading...</h1>}
-            {error && <h1>User not found</h1>}
-            <ul>
-                {data.map((user, index) => (
-                    <li key={index}>
-                        <div>
-                            <h1>"Username: "{user.name}</h1>
-                            <h2>"His logged like "{user.login}</h2>
-                            <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            {isError && <h1>User not found</h1>}
+            {user && (
+                <div>
+                    <h1>"Username: "{user.name}</h1>
+                    <h2>"His logged like "{user.login}</h2>
+                    <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
+                </div>
+            )}
         </>
-    )
+    );
 }
