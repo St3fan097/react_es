677a1917debeaf91011cfb60d88fe318937299b2
St3fan097 <Ste97.esse@gmail.com>
2023-11-29 12:11:35 +0100

    exercise-55

--- react-es_1/src/GithubUser.jsx
+++ react-es_1/src/GithubUser.jsx
@@ -22,7 +22,7 @@ export function GithubUser() {
     const { user, loading, isError, inputValue, setUsername, setInputValue } = useGithubUser();
 
     const fetchData = () => {
-        if (inputValue) {
+        if (inputValue !== null) {
             setUsername(inputValue);
         }
     };
